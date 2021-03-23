3ca5f39e47aef58d65899d30553933089df34e09
KalTist <2337105408@qq.com>
2021-03-23 18:06:52 +0800

    rm_runnable

--- 01.py
+++ 01.py
@@ -4,18 +4,26 @@ import pandas as pd
 def PreProcess(namestr):
     data = pd.read_csv(namestr+"_Raw_data.csv")
     # 去除首列的序号（linpack无序号）
-    # 去除尾列的场景
-    if not namestr=="linpack": data = data.iloc[:, 1:-1]
+    if not namestr=="linpack": data = data.iloc[:, 1:]
+    # 分离尾列的场景，方便后续纯数据处理
+    species = data["species"]
+    data = data.iloc[:, :-1]
     print("Has any null: ",(data.isnull()).any().any())
-    data.dropna(how="any")
+    # 经验证无缺失值
+    # data.dropna(how="any")
     # print(namestr+" reserved:")
     # print(data)
-    
-    data = (data - data.mean()) / data.std()
+    # NonGaussian = data["TasksTotal":"TasksSleeping"]
+    # 3σ原则筛选正常数据
+    normal = (data - data.mean()) / data.std()
+    normal = (normal >-3) & (normal < 3)
+    # 在原数据中保留正常数据
+    data = data[normal == True]
     print(data)
-    data = data[data > -3]
-    data = data[data < 3]
-    data = data.dropna(how="all")
+    # data["TasksTotal":"TasksRuning"] = NonGaussian
+    data = data.dropna(how="any")
+    # 补回尾列场景
+    data["species"] = namestr
     print(data)
     
 
